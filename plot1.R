8e1697a500896bcddbc46c1b433cf3ae04cc640b
domfernandez <dscf.279@gmail.com>
2014-06-07 19:23:59 -0400

    DF_7June_1923

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 # setwd("C://Users//Dominic//ExData_Plotting1//ExData_Plotting1")
 
 file_Dir_Name <- "C://Users//Dominic//ExData_Plotting1//household_power_consumption.txt"
-dataUsed <- read.table(fileName, header=T, sep=';', na.strings="?", 
+dataUsed <- read.table(file_Dir_Name, header=T, sep=';', na.strings="?", 
                        stringsAsFactors=T, skipNul=T)
 
 subDF = subset(dataUsed, as.Date(dataUsed$Date,"%d/%m/%Y") 
@@ -10,8 +10,7 @@ subDF = subset(dataUsed, as.Date(dataUsed$Date,"%d/%m/%Y")
                 == as.Date("02/02/2007","%d/%m/%Y")
               )
 
-dev.copy(png, file = "plot1.png")
-hist(subDF$Global_active_power, col = "red", xlab="Global Active Power",
+hist(subDF$Global_active_power, col = "red", xlab="Global Active Power (kilowatts)",
      main ="Global Active Power")
+dev.copy(png, file = "plot1.png")
 dev.off()
-
