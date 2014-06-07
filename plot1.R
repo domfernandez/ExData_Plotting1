38142af6a103498c8a2e9e2bf55c5a11e02386ed
domfernandez <dscf.279@gmail.com>
2014-06-07 18:55:30 -0400

    DF_7June_1855

--- plot1.R
+++ plot1.R
@@ -10,6 +10,8 @@ subDF = subset(dataUsed, as.Date(dataUsed$Date,"%d/%m/%Y")
                 == as.Date("02/02/2007","%d/%m/%Y")
               )
 
+dev.copy(png, file = "plot1.png")
 hist(subDF$Global_active_power, col = "red", xlab="Global Active Power",
      main ="Global Active Power")
-dev.copy(png, file = "plot1.png")
+dev.off()
+
