86d6269b25ab3d302183398040fae7f56701aa23
domfernandez <dscf.279@gmail.com>
2014-06-08 03:45:52 -0400

    DF_8June_0345

--- plot2.R
+++ plot2.R
@@ -1,15 +1,3 @@
-# setwd("C://Users//Dominic//ExData_Plotting1//ExData_Plotting1")
-
-# zip-file download-link
-# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-
-# Physical-file present at:
-# https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip
-
-# temp <- tempfile()
-# download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip",temp)
-# data <- read.table(unz(temp, "household_power_consumption.txt"))
-# unlink(temp)
 
 file_Dir_Name <- "C://Users//Dominic//ExData_Plotting1//household_power_consumption.txt"
 dataUsed <- read.table(file_Dir_Name, header=T, sep=';', na.strings="?", 
